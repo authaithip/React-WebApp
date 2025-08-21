009ed6713f07de94b45a1aeb1af7d118f55d8b67
authaithip <chanoonanauthaithip@gmail.com>
2025-08-21 17:49:56 +0700

    21/8

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <footer>&#169; {`${new Date().getFullYear()} - React Developer`}
-</footer>
-  )
+    <footer>&#169; {`${new Date().getFullYear()} - React Developer`}</footer>
+  );
 }
 
-export default Footer
+export default Footer;
---
+++ src/Pages/AboutPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <>
+      <div className="p-6 bg-white rounded-md shadow-sm">
+        <h1 className="font-bold text-2xl mb-3">About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fuga quo
+          corporis fugiat ea blanditiis sunt, delectus similique quis facilis
+          inventore tenetur molestias sit, pariatur eum eaque quaerat architecto
+          et. Rem!
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default AboutPage;
