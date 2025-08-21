3ab7307b869ce0a3d3dcc576705144aa0c7f0d13
authaithip <chanoonanauthaithip@gmail.com>
2025-08-21 15:17:25 +0700

    20/8

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>&#169; {`${new Date().getFullYear()} - React Developer`}
+</footer>
+  )
+}
+
+export default Footer
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import reactLogo from "../assets/react.svg";
+
+function Navbar() {
+  return (
+    <div>
+      <header>
+<div className="flex mr-auto gap-x-2 font-semibold text-2xl">
+<img src={reactLogo} alt="React Logo" />
+React : TODO List
+</div>
+</header>
+    </div>
+  )
+}
+
+export default Navbar
